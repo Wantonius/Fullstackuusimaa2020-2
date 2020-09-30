9d77d2cc4537d33e2dfa1361bf879f9a8a146717
Erno Hentonen <erno.hentonen@opiframe.com>
2020-09-30 11:17:33 +0300

    stateandlifecycle

--- react/stateandlifecycle/src/App.js
+++ react/stateandlifecycle/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HelloWorld from './HelloWorld';
+import StatefulComponent from './StatefulComponent';
 
 class App extends React.Component {
   render() {
 	  return (
 		<div className="App">
-			<HelloWorld name="Erno"/>
-			<HelloWorld/>
+			<StatefulComponent/>
 		</div>
 	  );
   }
