068925ada1b4543bbc037cf320866ee2c49e38cb
Erno Hentonen <erno.hentonen@opiframe.com>
2020-10-06 09:56:47 +0300

    react forms

---
+++ react/forms/src/NameForm.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default class NameForm extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			firstname:"",
+			lastname:""
+		}
+	}
+	
+	submit = (event) => {
+		event.preventDefault();
+		let name = this.state.firstname+" "+this.state.lastname;
+		this.props.setMessage(name);
+	}
+	
+	change = (event) => {
+		let state = {};
+		state[event.target.name] = event.target.value
+		this.setState(state);
+	}
+	
+	render() {
+		return(
+			<form onSubmit={this.submit}>
+				<label htmlFor="firstname">First Name:</label>
+				<input type="text"
+						name="firstname"
+						onChange={this.change}
+						value={this.state.firstname}/>
+				<br/>
+				<label htmlFor="lastname">Last Name:</label>
+				<input type="text"
+						name="lastname"
+						onChange={this.change}
+						value={this.state.lastname}/>
+				<br/>
+				<input type="submit" value="Send message"/>
+			</form>
+		)
+	}
+}
