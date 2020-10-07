c6653826bd9682360ece7f4f20071a965e44710a
Erno Hentonen <erno.hentonen@opiframe.com>
2020-10-07 14:22:12 +0300

    router test

---
+++ react/router_test/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Link,Switch,Route} from 'react-router-dom';
+import Home from './Home'
+import About from './About'
+import Secret from './Secret';
+
+class App extends React.Component {
+  render() {
+	  return (
+		<div className="App">
+			<ul>
+				<li><Link to="/">Home Page</Link></li>
+				<li><Link to="/about">About page</Link></li>
+			</ul>
+			<hr/>
+			<Switch>
+				<Route exact path="/" component={Home}/>
+				<Route path="/about" component={About}/>
+				<Route path="/secret" component={Secret}/>
+			</Switch>	
+		</div>
+	  );
+  }
+}
+
+export default App;
--- react/shoppingapp/2-stage-backend1/frontend/src/App.js
+++ react/shoppingapp/2-stage-backend1/frontend/src/App.js
@@ -9,33 +9,90 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			list:[],
-			id:500
+			list:[]
 		}
+	}
+	
+	componentDidMount() {
+		this.getList();
+	}
+	
+	getList = () => {
+		let request = {
+			method:"GET",
+			mode:"cors",
+			headers:{"Content-type":"application/json"}
+		}
+		fetch("/api/shopping",request).then(response => {
+			if(response.ok) {
+				response.json().then(data => {
+					this.setState({
+						list:data
+					})
+				}).catch(error => {
+					console.log("Error parsing JSON:",error);
+				});
+			} else {
+				console.log("Server responded with status:",response.status);
+			}
+		}).catch(error => {
+			console.log("Server responded with an error:",error);
+		});
 	}
 	
 	addToList = (item) => {
-		item.id = this.state.id;
-		let tempList = this.state.list.concat(item);
-		this.setState((state) =>{
-		return {
-			list:tempList,
-			id:state.id+1
+		let request = {
+			method:"POST",
+			mode:"cors",
+			headers:{"Content-type":"application/json"},
+			body:JSON.stringify(item)
 		}
-		})
+		fetch("/api/shopping",request).then(response => {
+			if(response.ok) {
+				this.getList();
+			}
+		    else {
+				console.log("Server responded with status:",response.status);
+			}
+		}).catch(error => {
+			console.log("Server responded with an error:",error);
+		});
 	}
 	
 	removeFromList = (id) => {
-		let tempList = this.state.list.filter(item => item.id !== id);
-		this.setState({
-			list:tempList
-		})
+		let request = {
+			method:"DELETE",
+			mode:"cors",
+			headers:{"Content-type":"application/json"}
+		}
+		fetch("/api/shopping/"+id,request).then(response => {
+			if(response.ok) {
+				this.getList();
+			}
+		    else {
+				console.log("Server responded with status:",response.status);
+			}
+		}).catch(error => {
+			console.log("Server responded with an error:",error);
+		});
 	}
 	
 	editItem = (newItem) => {
-		let tempList = this.state.list.map(item => item.id === newItem.id ? newItem : item)
-		this.setState({
-			list:tempList
+		let request = {
+			method:"PUT",
+			mode:"cors",
+			headers:{"Content-type":"application/json"},
+			body:JSON.stringify(newItem)
+		}
+		fetch("/api/shopping/"+newItem.id,request).then(response => {
+			if(response.ok) {
+				this.getList();
+			}
+		    else {
+				console.log("Server responded with status:",response.status);
+			}
+		}).catch(error => {
+			console.log("Server responded with an error:",error);
 		});
 	}
 	
