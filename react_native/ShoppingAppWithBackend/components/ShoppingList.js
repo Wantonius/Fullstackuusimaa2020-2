ed1021205f0ff586b467a03466c2f91acdb17cbe
Erno Hentonen <erno.hentonen@opiframe.com>
2021-02-19 10:59:08 +0200

    fetch stuff

--- react_native/ShoppingAppWithBackend/components/ShoppingList.js
+++ react_native/ShoppingAppWithBackend/components/ShoppingList.js
@@ -1,8 +1,74 @@
 import React from 'react';
 import StateManager from './context/StateManager';
+import {FlatList,View,Pressable,Text,StyleSheet} from 'react-native'
 
 class ShoppingList extends React.Component {
 
+	render() {
+		const styles = StyleSheet.create({
+			rowStyle:{
+				flexDirection:"row"
+			},
+			removeButton:{
+				width:80,
+				height:50,
+				backgroundColor:"#225522",
+				justifyContent:"center",
+				alignItems:"center"
+			},
+			editButton:{
+				width:80,
+				height:50,
+				backgroundColor:"#AA55AA",
+				justifyContent:"center",
+				alignItems:"center"
+			},
+			textStyle:{
+				fontSize:18,
+				fontFamily:"sans-serif",
+				fontWeight:"bold"
+			}
+		})
+	return(
+		<View>
+			<View style={styles.rowStyle}>
+				<Pressable onPress={() => this.props.logout()} 
+							style={styles.removeButton}>
+					<Text style={styles.textStyle}>Logout</Text>
+				</Pressable>
+				<Pressable onPress={() => this.props.navigation.navigate("Add Item")} 
+							style={styles.editButton}>
+					<Text style={styles.textStyle}>Add Item</Text>
+				</Pressable>			
+			</View>
+			<FlatList data={this.props.list}
+					renderItem={
+						({item,index}) => {
+							return(
+								<View style={style.rowStyle}>
+									<Text style={styles.textStyle}>
+										{item.count}
+									</Text>
+									<Text style={styles.textStyle}>
+										{item.type}
+									</Text>
+									<Text style={styles.textStyle}>
+										{item.price}
+									</Text>
+									<Pressable onPress={() => this.props.removeFromList(item.id)} 
+									style={styles.removeButton}>
+										<Text style={styles.textStyle}>Remove</Text>
+									</Pressable>
+									<Pressable style={styles.editButton}>
+										<Text style={styles.textStyle}>Edit</Text>
+									</Pressable>	
+								</View>
+							)
+						}
+					}/>
+		</View>
+	)
+	}
 }
 
 export default StateManager(ShoppingList);
--- react_native/ShoppingAppWithBackend/components/context/StateProvider.js
+++ react_native/ShoppingAppWithBackend/components/context/StateProvider.js
@@ -20,20 +20,118 @@ export default class StateProvider extends React.Component {
 		}
 	}
 	
+	setLoading = (loading) => {
+		this.setState({
+			loading:loading,
+			error:""
+		})
+	}
+	
+	setError = (error) => {
+		this.setState({
+			loading:false,
+			error:error
+		})
+	}
+	
 	login = (user) => {
-		
+		let request = {
+			method:"POST",
+			mode:"cors",
+			headers:{"Content-type":"application/json"},
+			body:JSON.stringify(user)
+		}
+		this.setLoading(true);
+		fetch("http://uusimaa-backend.herokuapp.com/login",request).then(response => {
+			if(response.ok) {
+				response.json().then(data => {
+					this.setState({
+						error:"",
+						loading:false,
+						token:data.token,
+						isLogged:true
+					},() => {
+						this.getShoppingList()
+					})
+				}).catch(error => {
+					this.setError("Failed to parse server response. Try again!");
+				})
+			} else {
+				let error = "Failed to log in. Server responded with status:"+response.status
+				this.setError(error);
+			}
+		}).catch(error => {
+			this.setError(error);
+		})		
 	}
 	
 	register = (user) => {
-		
+		let request = {
+			method:"POST",
+			mode:"cors",
+			headers:{"Content-type":"application/json"},
+			body:JSON.stringify(user)
+		}
+		this.setLoading(true);
+		fetch("http://uusimaa-backend.herokuapp.com/register",request).then(response => {
+			if(!response.ok) {
+				let error = "Register failed. Is username already in use? Server responded with status:"+response.status
+				this.setError(error);
+			} else {
+				this.setLoading(false);
+			}
+		}).catch(error => {
+			this.setError(error);
+		})
 	}
 	
 	logout = () => {
-		
+		let request = {
+			method:"POST",
+			mode:"cors",
+			headers:{"Content-type":"application/json",
+					"token":this.state.token}
+		}
+		this.setLoading(true);
+		fetch("http://uusimaa-backend.herokuapp.com/logout",request).then(response => {
+			this.setState({
+				list:[],
+				error:"",
+				loading:false,
+				isLogged:false,
+				token:""
+			})
+		}).catch(error => {
+			this.setError(error);
+		})		
 	}
 	
 	getShoppingList = () => {
-		
+		let request = {
+			method:"GET",
+			mode:"cors",
+			headers:{"Content-type":"application/json",
+					 "token":this.state.token}
+		}
+		this.setLoading(true);
+		fetch("http://uusimaa-backend.herokuapp.com/api/shopping",request).then(response => {
+			if(response.ok) {
+				response.json().then(data => {
+					this.setState({
+						error:"",
+						loading:false,
+						list:data
+					})
+				}).catch(error => {
+					this.setError("Failed to parse server response. Try again!");
+				})
+			} else {
+				let error = "Failed to load resources. Server responded with status:"+response.status
+				this.setError(error);
+			}
+		}).catch(error => {
+			this.setError(error);
+		})			
 	}
 	
 	addToList = (item) => {
@@ -41,6 +139,10 @@ export default class StateProvider extends React.Component {
 	}
 	
 	removeFromList = (id) => {
+		
+	}
+	
+	editItem = (item) => {
 		
 	}
 	
@@ -54,6 +156,7 @@ export default class StateProvider extends React.Component {
 				list:this.state.list,
 				addToList:this.addToList,
 				removeFromList:this.removeFromList,
+				editItem:this.editItem,
 				login:this.login,
 				register:this.register,
 				logout:this.logout
